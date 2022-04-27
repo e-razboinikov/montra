df52d2686954089b797915480b4bbe56750fdf6b
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-04-27 10:24:02 +0500

    Router added.

---
+++ lib/internal/routing.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      // name: SplashScreenPage.name,
+      // builder: (context, state) => const SplashScreenPage(),
+    ),
+  ],
+);
