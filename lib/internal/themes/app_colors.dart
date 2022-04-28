3d3a6954e631335d8693537b49d536d7e289016c
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-04-28 09:52:59 +0500

    App colors added.

---
+++ lib/internal/themes/app_colors.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Dark
+  static const Color dark100 = Color(0x0D0E0F);
+  static const Color dark75 = Color(0x161719);
+  static const Color dark50 = Color(0x464A4D);
+  static const Color dark25 = Color(0x7A7E80);
+
+  // Light
+  static const Color light100 = Color(0xFFFFFF);
+  static const Color light80 = Color(0xFCFCFC);
+  static const Color light60 = Color(0xF1F1FA);
+  static const Color light40 = Color(0xE3E5E5);
+  static const Color light20 = Color(0x91919F);
+
+  // Violet
+  static const Color violet100 = Color(0x7F3DFF);
+  static const Color violet80 = Color(0x8F57FF);
+  static const Color violet60 = Color(0xB18AFF);
+  static const Color violet40 = Color(0xD3BDFF);
+  static const Color violet20 = Color(0xEEE5FF);
+
+  // Red
+  static const Color red100 = Color(0xFD3C4A);
+  static const Color red80 = Color(0xFD5662);
+  static const Color red60 = Color(0xFD6F7A);
+  static const Color red40 = Color(0xFDA2A9);
+  static const Color red20 = Color(0xFDD5D7);
+
+  // Green
+  static const Color green100 = Color(0x00A86B);
+  static const Color green80 = Color(0x2AB784);
+  static const Color green60 = Color(0x65D1AA);
+  static const Color green40 = Color(0x93EACA);
+  static const Color green20 = Color(0xCFFAEA);
+
+  // Yellow
+  static const Color yellow100 = Color(0xFCAC12);
+  static const Color yellow80 = Color(0xFCBB3C);
+  static const Color yellow60 = Color(0xFCCC6F);
+  static const Color yellow40 = Color(0xFCDDA1);
+  static const Color yellow20 = Color(0xFCEED4);
+
+  // Blue
+  static const Color blue100 = Color(0x0077FF);
+  static const Color blue80 = Color(0x248AFF);
+  static const Color blue60 = Color(0x57A5FF);
+  static const Color blue40 = Color(0x8AC0FF);
+  static const Color blue20 = Color(0xBDDCFF);
+}
