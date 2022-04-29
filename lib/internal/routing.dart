9d4251d3fdaadec78159182dff364995c15f9ccf
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-04-29 11:38:38 +0500

    BotToast package initialization added.

--- lib/internal/routing.dart
+++ lib/internal/routing.dart
@@ -1,12 +1,15 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:go_router/go_router.dart';
+import 'package:montra/features/onboarding_screen/presentation/pages/onboarding_page.dart';
 import 'package:montra/features/splash_screen/presentation/pages/splash_screen_page.dart';
 
 final router = GoRouter(
+  observers: [BotToastNavigatorObserver()],
   routes: [
     GoRoute(
       path: '/',
       name: SplashScreenPage.name,
-      builder: (context, state) => const SplashScreenPage(),
+      builder: (context, state) => OnboardingPage(),
     ),
   ],
 );
