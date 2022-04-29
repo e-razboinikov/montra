781747ba9d5a666e353a68e84f728188110ca111
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-04-29 11:39:04 +0500

    Onboarding images added to resources.

---
+++ lib/internal/resources/graphics_resources/raster_resources.dart
@@ -0,0 +1,9 @@
+class RasterResources {
+  // Onboarding images:
+  static const String obloardingFirstImage =
+      'assets/graphics/raster/onboarding_images/onboarding_first_image.png';
+  static const String obloardingSecondImage =
+      'assets/graphics/raster/onboarding_images/onboarding_second_image.png';
+  static const String obloardingThirdImage =
+      'assets/graphics/raster/onboarding_images/onboarding_third_image.png';
+}
