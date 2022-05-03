f9033133c2673bc64a0db286a6e037b8302530d2
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-03 12:00:42 +0500

    Added onoarding use cases.

---
+++ lib/features/onboarding_screen/domain/use_cases/onboarding_use_cases.dart
@@ -0,0 +1,15 @@
+import 'package:montra/features/onboarding_screen/domain/repositories/onboarding_repo.dart';
+
+class OnboardingUseCases {
+  const OnboardingUseCases({required this.repo});
+
+  final OnboardingRepo repo;
+
+  Future<bool> checkShowing() async {
+    return await repo.checkShowing();
+  }
+
+  Future<void> markAsShowed() async {
+    await repo.checkShowing();
+  }
+}
