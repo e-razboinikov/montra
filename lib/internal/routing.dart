36e277d37cbd128e6c6e941015a46dbe1d490e42
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-04 15:49:48 +0500

    Routing corrected.

--- lib/internal/routing.dart
+++ lib/internal/routing.dart
@@ -1,5 +1,6 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:go_router/go_router.dart';
+import 'package:montra/features/main_screen/presentation/pages/main_page.dart';
 import 'package:montra/features/onboarding_screen/presentation/pages/onboarding_page.dart';
 import 'package:montra/features/start_page/presentation/start_page.dart';
 
@@ -11,12 +12,19 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       name: StartPage.name,
-      builder: (context, state) => StartPage(),
+      builder: (context, state) => const StartPage(),
+      routes: [
+        GoRoute(
+          path: 'onboarding',
+          name: OnboardingPage.name,
+          builder: (context, state) => OnboardingPage(),
+        ),
+      ],
     ),
     GoRoute(
-      path: '/onboarding',
-      name: OnboardingPage.name,
-      builder: (context, state) => OnboardingPage(),
+      path: '/main',
+      name: MainPage.name,
+      builder: (context, state) => const MainPage(),
     ),
   ],
 );
