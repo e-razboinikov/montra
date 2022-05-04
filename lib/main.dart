f1c60fcaa0b89ab7df88166c404347ca4cc9e34c
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-04 15:49:06 +0500

    Initialization added.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:montra/features/onboarding_screen/presentation/bloc/onboarding_bloc.dart';
+import 'package:montra/features/onboarding_screen/presentation/pages/onboarding_page.dart';
 import 'package:montra/internal/bloc_providers.dart';
 import 'package:montra/internal/routing.dart';
 import 'package:montra/internal/themes/app_themes.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Hive.initFlutter();
 
   runApp(
