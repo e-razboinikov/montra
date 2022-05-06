a7c03bd08ac0a7ff47017c633c371b449a4495e8
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-06 12:47:25 +0500

    Corrected the layout of the Onboarding Screen.

--- lib/features/onboarding_screen/presentation/pages/onboarding_page.dart
+++ lib/features/onboarding_screen/presentation/pages/onboarding_page.dart
@@ -48,68 +48,77 @@ class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.65,
-              child: PageView(
-                scrollBehavior: const CupertinoScrollBehavior(),
-                controller: _controller,
-                onPageChanged: (position) =>
-                    setState(() => _currentPage = position.toDouble()),
-                children: _onboardigInfo
-                    .map(
-                      (e) => OnboardingItem(onboardingInfo: e),
-                    )
-                    .toList(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: DotsIndicator(
-                dotsCount: _onboardigInfo.length,
-                position: _currentPage,
-                decorator: const DotsDecorator(
-                  size: Size(8.0, 8.0),
-                  activeSize: Size(16.0, 16.0),
-                  color: AppColors.violet20,
-                  activeColor: AppColors.violet100,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraint) => SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraint.maxHeight),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: constraint.maxHeight * 0.7,
+                      child: PageView(
+                        scrollBehavior: const CupertinoScrollBehavior(),
+                        controller: _controller,
+                        onPageChanged: (position) =>
+                            setState(() => _currentPage = position.toDouble()),
+                        children: _onboardigInfo
+                            .map(
+                              (e) => OnboardingItem(onboardingInfo: e),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      child: DotsIndicator(
+                        dotsCount: _onboardigInfo.length,
+                        position: _currentPage,
+                        decorator: const DotsDecorator(
+                          size: Size(8.0, 8.0),
+                          activeSize: Size(16.0, 16.0),
+                          color: AppColors.violet20,
+                          activeColor: AppColors.violet100,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32.0, 0, 32.0, 32.0),
+                      child: Column(
+                        children: [
+                          CoreButton(
+                            buttonText: 'Sign Up',
+                            onPressed: () => BotToast.showText(
+                              text: 'Here gonna be sign up logic!',
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          CoreButton(
+                            buttonText: 'Login',
+                            onPressed: () {
+                              context.read<OnboardingBloc>().add(
+                                    const ShowingToggleOnboardingEvent(),
+                                  );
+
+                              context.goNamed(MainPage.name);
+
+                              BotToast.showText(
+                                text: 'Here gonna be sign in logic!',
+                              );
+                            },
+                            primary: AppColors.violet20,
+                            onPrimary: AppColors.violet100,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                children: [
-                  CoreButton(
-                    buttonText: 'Sign Up',
-                    onPressed: () => BotToast.showText(
-                      text: 'Here gonna be sign up logic!',
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  CoreButton(
-                    buttonText: 'Login',
-                    onPressed: () {
-                      context.read<OnboardingBloc>().add(
-                            const ShowingToggleOnboardingEvent(),
-                          );
-
-                      context.goNamed(MainPage.name);
-
-                      BotToast.showText(
-                        text: 'Here gonna be sign in logic!',
-                      );
-                    },
-                    primary: AppColors.violet20,
-                    onPrimary: AppColors.violet100,
-                  ),
-                ],
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
