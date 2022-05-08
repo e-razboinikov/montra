0e52ef8d3bc3e4b3132fedb2730c4efb1caec9de
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-08 10:04:13 +0500

    Changed the name of the first screen selector.

--- lib/internal/routing.dart
+++ lib/internal/routing.dart
@@ -1,8 +1,8 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:go_router/go_router.dart';
+import 'package:montra/features/first_screen_selecter/presentation/first_screen_selecter.dart';
 import 'package:montra/features/main_screen/presentation/pages/main_page.dart';
 import 'package:montra/features/onboarding_screen/presentation/pages/onboarding_page.dart';
-import 'package:montra/features/start_page/presentation/start_page.dart';
 
 final router = GoRouter(
   observers: [
@@ -11,8 +11,8 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      name: StartPage.name,
-      builder: (context, state) => const StartPage(),
+      name: FirstScreenSelecter.name,
+      builder: (context, state) => const FirstScreenSelecter(),
       routes: [
         GoRoute(
           path: 'onboarding',
