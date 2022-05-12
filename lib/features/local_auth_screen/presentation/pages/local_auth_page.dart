9e7bc2b88c7aeef10fb35863f2f4aa948cccbca6
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-12 11:06:59 +0500

    Improved code of Local Auth Page.

--- lib/features/local_auth_screen/presentation/pages/local_auth_page.dart
+++ lib/features/local_auth_screen/presentation/pages/local_auth_page.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:montra/features/local_auth_screen/presentation/widgets/local_auth_keyboard.dart';
 import 'package:montra/features/local_auth_screen/presentation/widgets/pin_code_field.dart';
+import 'package:montra/internal/l10n/generated/l10n.dart';
 import 'package:montra/internal/themes/app_colors.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class LocalAuthPage extends StatefulWidget {
-  LocalAuthPage({Key? key}) : super(key: key);
+  /// Local authorization page with PIN.
+  const LocalAuthPage({Key? key}) : super(key: key);
 
   static const name = 'localAuth';
 
@@ -14,13 +19,24 @@ class LocalAuthPage extends StatefulWidget {
 
 class _LocalAuthPageState extends State<LocalAuthPage> {
   final TextEditingController _textEditingController = TextEditingController();
+  // ignore: close_sinks
+  final StreamController<ErrorAnimationType> _errorController =
+      StreamController<ErrorAnimationType>();
+
+  late final double _deviceHeight;
+  late final Locales _locales;
 
   ValueNotifier<String> currentText = ValueNotifier('');
 
   @override
-  Widget build(BuildContext context) {
-    final double _deviceHeight = MediaQuery.of(context).size.height;
+  void didChangeDependencies() {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _locales = Locales.of(context);
+    super.didChangeDependencies();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.violet100,
       body: Column(
@@ -29,14 +45,17 @@ class _LocalAuthPageState extends State<LocalAuthPage> {
           Expanded(
             flex: _deviceHeight >= 720 ? 3 : 2,
             child: PinCodeField(
-              currentText: currentText,
-              controller: _textEditingController,
+              textController: _textEditingController,
+              errorController: _errorController,
+              locales: _locales,
             ),
           ),
           Expanded(
             flex: _deviceHeight >= 720 ? 2 : 3,
             child: LocalAuthKeyboard(
-              controller: _textEditingController,
+              textController: _textEditingController,
+              deviceHeight: _deviceHeight,
+              locales: _locales,
             ),
           ),
         ],
