11f7f89a3442d0adc0e027d4af3c674927e93ff9
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-17 14:45:56 +0500

    Local Auth Bloc in BlocProvider now.

--- lib/internal/bloc_providers.dart
+++ lib/internal/bloc_providers.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:montra/features/local_auth_screen/data/data_sources/services/local_auth_service.dart';
+import 'package:montra/features/local_auth_screen/data/repositories/local_auth_repo_impl.dart';
+import 'package:montra/features/local_auth_screen/domain/use_cases/local_auth_use_cases.dart';
+import 'package:montra/features/local_auth_screen/presentation/bloc/local_auth_bloc.dart';
 import 'package:montra/features/onboarding_screen/data/data_sources/services/onboarding_service.dart';
 import 'package:montra/features/onboarding_screen/data/repositories/onboarding_repo_impl.dart';
 import 'package:montra/features/onboarding_screen/domain/use_cases/onboarding_use_cases.dart';
@@ -10,6 +14,15 @@ List<BlocProvider> get providers => [
           useCases: OnboardingUseCases(
             repo: OnboardingRepoImpl(
               service: OnboardingSevice(),
+            ),
+          ),
+        ),
+      ),
+      BlocProvider<LocalAuthBloc>(
+        create: (context) => LocalAuthBloc(
+          useCases: LocalAuthUseCases(
+            repo: LocalAuthRepoImpl(
+              service: LocalAuthService(),
             ),
           ),
         ),
