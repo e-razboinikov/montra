742e2cffb23386a176889f505904e426c8c06aa3
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-17 14:44:28 +0500

    Corrected Local Auth Use Cases.

--- lib/features/local_auth_screen/domain/use_cases/local_auth_use_cases.dart
+++ lib/features/local_auth_screen/domain/use_cases/local_auth_use_cases.dart
@@ -1,15 +1,11 @@
-import 'package:montra/features/local_auth_screen/domain/entities/local_auth_pin_confirm_entity.dart';
 import 'package:montra/features/local_auth_screen/domain/repositories/local_auth_repo.dart';
-import 'package:montra/internal/l10n/generated/l10n.dart';
 
 class LocalAuthUseCases {
   const LocalAuthUseCases({
     required this.repo,
-    required this.locales,
   });
 
   final LocalAuthRepo repo;
-  final Locales locales;
 
   /// Retrieve a previously saved pin.
   Future<String?> getStoredPinOrNull() async => repo.getStoredPinOrNull();
@@ -18,24 +14,9 @@ class LocalAuthUseCases {
   Future<void> storePin(String pinToStore) async => repo.storePin(pinToStore);
 
   /// Checks the validity of the new PIN and sends an error message if necessary.
-  LocalAuthPinConfirmEntity confirmPin({
+  bool confirmPin({
     required String oldPin,
     required String newPin,
-  }) {
-    if (newPin.length != 4) {
-      return LocalAuthPinConfirmEntity(
-        isPinValid: false,
-        errorMessage: locales.thePinLengthMustBe4,
-      );
-    }
-
-    if (newPin != oldPin) {
-      return LocalAuthPinConfirmEntity(
-        isPinValid: false,
-        errorMessage: locales.theNewPinDoesNotMatchTheOldOnePlease,
-      );
-    }
-
-    return const LocalAuthPinConfirmEntity(isPinValid: true);
-  }
+  }) =>
+      newPin.length == 4 && newPin == oldPin;
 }
