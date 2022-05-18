9e73a483cac31fe3e5be1a79c22e4588465384b7
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-18 09:51:17 +0500

    Added FaceID icon. Name corrected.

--- lib/core/resources/vector_resources.dart
+++ lib/core/resources/vector_resources.dart
@@ -1,5 +1,6 @@
 class VectorResources {
   // Icons:
   static const String iconArrowRight =
-      'assets/graphics/vector/icons/arrow-right.svg';
+      'assets/graphics/vector/icons/arrow_right.svg';
+  static const String faceId = 'assets/graphics/vector/icons/face_id.svg';
 }
