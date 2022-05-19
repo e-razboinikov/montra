68ab48f2fac5312a440058133e5397c83f879cf6
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-19 13:50:01 +0500

    Setup Account Page added.

---
+++ lib/features/account/account_management/presentation/pages/setup_account_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:montra/core/themes/app_colors.dart';
+import 'package:montra/core/themes/app_text_styles.dart';
+import 'package:montra/core/widgets/buttons/core_button.dart';
+import 'package:montra/internal/localization/generated/l10n.dart';
+
+class SetupAccountPage extends StatelessWidget {
+  const SetupAccountPage({Key? key}) : super(key: key);
+
+  static const String name = 'setupAccount';
+
+  @override
+  Widget build(BuildContext context) {
+    final locales = Locales.of(context);
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(16.h),
+          child: Column(
+            children: [
+              SizedBox(height: 51.h),
+              Text(
+                locales.letsSetupYourAccount,
+                style: ag.copyWith(
+                  color: AppColors.dark50,
+                ),
+              ),
+              SizedBox(height: 37.h),
+              Text(
+                locales.accountCanBeYourBankCardOrYourWallet,
+                style: body3.copyWith(
+                  color: AppColors.dark50,
+                ),
+              ),
+              const Spacer(),
+              CoreButton(
+                buttonText: 'Let’s go',
+                onPressed: () => null,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
