f34f936ab310f9867c135cfa3a78a4a4aed26424
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-20 15:21:27 +0500

    Style improvement.

--- lib/features/local_auth_screen/presentation/bloc/local_auth_bloc.dart
+++ lib/features/local_auth_screen/presentation/bloc/local_auth_bloc.dart
@@ -3,7 +3,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:montra/features/local_auth_screen/domain/use_cases/local_auth_use_cases.dart';
 
 part 'local_auth_bloc.freezed.dart';
+
 part 'local_auth_event.dart';
+
 part 'local_auth_state.dart';
 
 class LocalAuthBloc extends Bloc<LocalAuthEvent, LocalAuthState> {
