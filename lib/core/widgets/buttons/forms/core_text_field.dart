39bd31c9aa694bea156baea21e3d4acdca986da1
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-20 15:19:32 +0500

    Added two base forms.

---
+++ lib/core/widgets/buttons/forms/core_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:montra/core/themes/app_colors.dart';
+import 'package:montra/core/themes/app_text_styles.dart';
+
+class CoreTextField extends StatelessWidget {
+  const CoreTextField({
+    required this.name,
+    required this.label,
+    Key? key,
+  }) : super(key: key);
+
+  final String name;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+      name: name,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.w),
+          borderSide: BorderSide(color: AppColors.light60, width: 1.w),
+        ),
+        label: Text(
+          label,
+          style: body1.copyWith(color: AppColors.light20),
+        ),
+      ),
+    );
+  }
+}
