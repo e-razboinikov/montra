0be9cb893857877d885aa4500d9a6affe3d06a36
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-20 15:18:15 +0500

    Corrected setup account page.

--- lib/features/account/account_management/presentation/pages/setup_account_page.dart
+++ lib/features/account/account_management/presentation/pages/setup_account_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:montra/core/themes/app_colors.dart';
 import 'package:montra/core/themes/app_text_styles.dart';
 import 'package:montra/core/widgets/buttons/core_button.dart';
+import 'package:montra/features/account/account_management/presentation/pages/account_management_page.dart';
 import 'package:montra/internal/localization/generated/l10n.dart';
 
 class SetupAccountPage extends StatelessWidget {
@@ -35,8 +37,8 @@ class SetupAccountPage extends StatelessWidget {
               ),
               const Spacer(),
               CoreButton(
-                buttonText: 'Let’s go',
-                onPressed: () => null,
+                buttonText: locales.letsGo,
+                onPressed: () => context.goNamed(AccountManagementPage.name),
               ),
             ],
           ),
