1c567cb05df05f07a0f488e64d100e7659fe8ea4
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-28 11:47:32 +0500

    Account entity corrected.

--- lib/features/account/account_management/domain/entities/account_entity.dart
+++ lib/features/account/account_management/domain/entities/account_entity.dart
@@ -2,17 +2,20 @@ import 'package:equatable/equatable.dart';
 
 class AccountEntity extends Equatable {
   const AccountEntity({
+    required this.id,
     required this.name,
     required this.type,
     required this.balance,
   });
 
+  final int id;
   final String name;
   final String type;
   final double balance;
 
   @override
   List<Object?> get props => [
+        id,
         name,
         type,
         balance,
