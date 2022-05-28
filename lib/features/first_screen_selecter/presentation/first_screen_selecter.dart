b0e6bfe77bf0712de53487dcfad16e0cda51a775
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-28 11:53:15 +0500

    Hive adapters registration in app initialization now.

--- lib/features/first_screen_selecter/presentation/first_screen_selecter.dart
+++ lib/features/first_screen_selecter/presentation/first_screen_selecter.dart
@@ -3,12 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:go_router/go_router.dart';
 import 'package:montra/features/account/account_management/presentation/pages/setup_account_page.dart';
-import 'package:montra/features/account/account_management/presentation/pages/setup_account_success_page.dart';
-import 'package:montra/features/home/presentation/pages/main_page.dart';
 import 'package:montra/features/local_auth/presentation/bloc/local_auth_bloc.dart';
 import 'package:montra/features/local_auth/presentation/pages/local_auth_page.dart';
 import 'package:montra/features/onboarding/presentation/bloc/onboarding_bloc.dart';
 import 'package:montra/features/onboarding/presentation/pages/onboarding_page.dart';
+import 'package:montra/internal/hive_adapters_registrant.dart';
 
 class FirstScreenSelecter extends StatefulWidget {
   /// This page is necessary for the correct selection of the desired page after
@@ -29,6 +28,8 @@ class _FirstScreenSelecterState extends State<FirstScreenSelecter> {
   }
 
   Future<void> _prepareApp() async {
+    registerHiveAdapters();
+
     context.read<OnboardingBloc>().add(
           const CheckShowingOnboardingEvent(),
         );
