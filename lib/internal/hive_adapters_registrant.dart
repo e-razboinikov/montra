65ef97ba691d0dcc083ae19c9ff889e5a462137b
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-05-29 18:20:37 +0500

    Hive adapters registrant corrected.

--- lib/internal/hive_adapters_registrant.dart
+++ lib/internal/hive_adapters_registrant.dart
@@ -1,10 +1,7 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:montra/features/account/account_management/data/models/account_model.dart';
-import 'package:montra/features/account/account_management/data/models/accounts_model.dart';
 
 void registerHiveAdapters() {
   // id: 0
   Hive.registerAdapter<AccountModel>(AccountModelAdapter());
-  // id: 1
-  Hive.registerAdapter<AccountsModel>(AccountsModelAdapter());
 }
