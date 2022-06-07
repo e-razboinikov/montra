eb68f473827fd21feecfaae3ad358095fccbb9df
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-06-07 20:20:22 +0500

    Removed unused import.

--- lib/features/account/account_management/domain/repositories/account_repo.dart
+++ lib/features/account/account_management/domain/repositories/account_repo.dart
@@ -1,4 +1,3 @@
-import 'package:montra/features/account/account_management/data/models/account_model.dart';
 import 'package:montra/features/account/account_management/domain/entities/account_entity.dart';
 
 abstract class AccountRepo {
