9ebd794af7a411c5251bcd4bea79f311455ab63f
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-06-08 17:51:31 +0500

    Main Page improved.

--- lib/features/home/presentation/pages/main_page.dart
+++ lib/features/home/presentation/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:montra/core/themes/app_colors.dart';
 import 'package:montra/core/themes/app_text_styles.dart';
 import 'package:montra/internal/localization/generated/l10n.dart';
@@ -24,11 +25,19 @@ class MainPage extends StatelessWidget {
             ),
           ),
         ),
-        body: Center(
-          child: Text(
-            locales.applicationStillInWork,
-            style: body1,
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.access_time_outlined,
+              size: 256.h,
+            ),
+            SizedBox(height: 32.h),
+            Text(
+              locales.applicationStillInWork,
+              style: body1,
+            ),
+          ],
         ),
       ),
     );
